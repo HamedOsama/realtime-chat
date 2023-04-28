239c718a353ef5499d1fb64d8645e24b6ae790db
Hamed Osama <hamedosama19@gmail.com>
2023-04-28 21:50:01 +0200

    delete user data from dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -8,7 +8,8 @@ import { FC } from 'react'
 const page = async ({ }) => {
   const session = await getServerSession(authOptions)
   return <pre>
-    {JSON.stringify(session)}
+    {/* {JSON.stringify(session)} */}
+    Dashboard
   </pre>
 }
 
