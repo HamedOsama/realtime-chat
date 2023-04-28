381326bbcf3af95b3e0c516d7a3126f07799538d
Hamed Osama <hamedosama19@gmail.com>
2023-04-28 18:26:15 +0200

    format file

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -11,21 +11,21 @@ const page = async ({ }) => {
 
   const incomingSenderIds = await fetchRedisData('smembers', `user:${session.user.id}:incoming_friend_requests`) as string[];
 
-  const incomingFriendRequests : IncomingFriendRequests[] = await Promise.all(
+  const incomingFriendRequests: IncomingFriendRequests[] = await Promise.all(
     incomingSenderIds.map(async (id) => {
       const user = await fetchRedisData('get', `user:${id}`) as string;
       const parsedUser = JSON.parse(user) as User;
       return {
-        senderId : id,
-        senderEmail : parsedUser.email,
-        senderImage : parsedUser.image
+        senderId: id,
+        senderEmail: parsedUser.email,
+        senderImage: parsedUser.image
       };
     })
   )
   return <main className='pt-8 px-4 sm:px-8'>
     <h1 className='font-bold text-5xl mb-8'>Friend Requests</h1>
     <div className="flex flex-col gap-4">
-      <FriendRequests sessionId={session.user.id} incomingFriendRequests={incomingFriendRequests}/>
+      <FriendRequests sessionId={session.user.id} incomingFriendRequests={incomingFriendRequests} />
     </div>
   </main>
 }
