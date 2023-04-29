ab00b2fabb0f87631af521e9021de526f56fc56b
Hamed Osama <hamedosama19@gmail.com>
2023-04-29 23:15:45 +0200

    create chat layout

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import { messageArrayValidator } from '@/lib/Validations/message'
 import { authOptions } from '@/lib/auth'
 import db from '@/lib/db'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
 
@@ -12,21 +13,21 @@ interface pageProps {
   }
 }
 
-async function getChatMessage(chatId : string){
+async function getChatMessage(chatId: string) {
   try {
-    const results : string[] = await fetchRedisData(
+    const results: string[] = await fetchRedisData(
       'zrange',
       `chat:${chatId}:messages`,
       0,
       -1
-      ) as string[]
-      const dbMessages = results.map((message) => JSON.parse(message) as Message);
+    ) as string[]
+    const dbMessages = results.map((message) => JSON.parse(message) as Message);
 
-      const reversedMessages = dbMessages.reverse();
+    const reversedMessages = dbMessages.reverse();
 
-      const messages = messageArrayValidator.parse(reversedMessages);
+    const messages = messageArrayValidator.parse(reversedMessages);
 
-      return messages;
+    return messages;
   } catch (e) {
     notFound()
   }
@@ -47,10 +48,34 @@ const page = async ({ params }: pageProps) => {
 
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
 
-  const chatPartner = (await (db.get(`user:${chatPartnerId}`) as Promise<User>) ) as User;
+  const chatPartner = (await (db.get(`user:${chatPartnerId}`) as Promise<User>)) as User;
 
   const initialMessage = await getChatMessage(chatId);
-  return <div>{params.chatId}</div>
+  return <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+    <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+      <div className="relative flex items-center space-x-4">
+        <div className="relative">
+          <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+            <Image
+              src={chatPartner.image}
+              alt={`${chatPartner.name}'s profile picture`}
+              fill
+              referrerPolicy='no-referrer'
+              className='rounded-full'
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col leading-tight">
+          <div className="text-xl flex items-center">
+            <span className='text-gray-700 mr-3 font-semibold'>{chatPartner.name}</span>
+          </div>
+          <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
 }
 
 export default page
